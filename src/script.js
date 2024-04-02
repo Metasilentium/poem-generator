36b392a4fe51cb6ebe38c78b25399987768a9927
Metasilentium <148706571+Metasilentium@users.noreply.github.com>
2024-04-02 16:02:01 -0400

    added extra context

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "0907b33b9f9occ3f077738b8ba754tb7";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem expert and you love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions. Do not include a title to the poem. Sign your poem with: 'SheCodes AI' inside a <strong> element at the end of the poem. Do not leave space at the bottom.";
+    "You are a romantic poem expert and you love to write short poems. Your mission is to generate a 4 line poem in basic HTML, separating each line of the poem with <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign your poem with: 'SheCodes AI' inside a <strong> element at the end of the poem. Do not leave space at the bottom.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
